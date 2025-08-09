d9e9effeef00a10eb7e8551358769207538ec64e
damodar-d <abhiram.deshpande00@gmail.com>
2025-08-09 11:23:44 +0530

    added main code

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
+    string playerName;
+    
+    cout << "Enter your name: ";
+    cin >> playerName;
 
+    cout << "Great Start " << playerName << ", looks like you have understood the main() function properly now!" << endl;
+
+    return 0;
 }
